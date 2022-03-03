e05c303a4709bc969555eceb19ccc53bbe590e0b
Md shohel Mia <shohel.uoda@engineer.com>
2022-03-03 20:58:00 +0600

    variable code send to github

---
+++ variable.js
@@ -0,0 +1,17 @@
+
+//---------------------------------------- Variable -----------------
+const num1 = 100;
+const num2 = 50;
+const sum = num1 + num2
+const sub = num1 - num2
+const mul = num1 * num2
+const div = num1 / num2
+const rem = num1 % num2
+
+//-------------------------- Print -------------------------
+
+console.log("Summation : ",sum);
+console.log("Subtraction : ",sub)
+console.log("Multiplication : ",mul);
+console.log("Division : ",div);
+console.log("Remainder ",rem);
